ba3dfbd846e02d1457062bc103f08b095f8f2916
Mike Hohne <mikehohne@gmail.com>
2017-07-30 17:28:25 -0600

    added some bootstrap for syling purposes

--- src/BookList.js
+++ src/BookList.js
@@ -10,15 +10,20 @@ class BookList extends Component {
     console.log(books)
     return (
       <div className='list-group'>
+        <h1>My Bookshelf</h1>
         <ul className='books-list'>
           {books.map((book) =>(
             <li key={book.id} className='list-group-item'>
                 <h2 className='list-group-item-heading'>{book.title}</h2>
                 <p className='list-group-item-text'>{book.subtitle}</p>
-                <img src={book.imageLinks.thumbnail} alt='bookThumb' />
+                <img className='img-thumbnail' src={book.imageLinks.thumbnail} alt='bookThumb' />
               {book.authors.map((author) => (
                 <p key={author} className='list-group-item-text'>{author}</p>
               ))}
+              <br />
+              <a href={'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + book.title}>
+                <button className='btn btn-success'>Purchase</button>
+              </a>
             </li>
           ))}
         </ul>
